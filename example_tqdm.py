60838a056f08b5cffc939b524300c3731d129716
Julian Garcia <juliancatorce@gmail.com>
2019-03-07 00:03:04 -0600

    Updated: example_tqdm, with  set to True and  changed to a bigger pdf file

--- example_tqdm.py
+++ example_tqdm.py
@@ -1,14 +1,15 @@
-from tqdm import tqdm
+import pdb
+
 import requests
+from tqdm import tqdm
 
-url = 'https://www.fing.edu.uy/~canale/latex.pdf'
+url = 'https://cdn-10.nikon-cdn.com/pdf/manuals/dslr/D90_es.pdf'
 
-req = requests.get(url)
+req = requests.get(url, stream=True, headers={'Accept-Encoding': None})
 
 file_size = int(req.headers['Content-length'])
 filename = "aname.pdf" # req.headers['Content-Disposition']
 chunk_size = 1024
 with open(filename, 'wb') as fw:
-    for chunck in tqdm(iterable= req.iter_content(chunk_size=chunk_size), total = file_size/chunk_size, unit = 'kb'):
+    for chunck in tqdm(iterable= req.iter_content(chunk_size=chunk_size), total = file_size/chunk_size, unit = 'KB'):
         fw.write(chunck)
-
